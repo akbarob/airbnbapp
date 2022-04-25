f20abd2e05f8f08fc0bdb652fbf0f2373944ee8c
Akbar <akbarbadmus07@gmail.com>
2022-04-25 12:10:40 +0100

    cat props done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Cards from './Components/card';
+import Contacts from './Components/Contact';
 import Header from './Components/header';
 import Hero from './Components/hero';
 
@@ -12,6 +13,35 @@ function App() {
       <div className='container'>
         <Hero/>
         <Cards/>
+        <div className='row'>
+          <Contacts
+            img={require('./images/cat 1.jpg')}
+            name="Mr WHizkitson"
+            phoneno='(212) 555-1234'
+            email="mr.whizkitson@meow.com"
+          />
+          <Contacts
+          img={require('./images/reing.jpg')}
+          name="Mr Fluffykit"
+          phoneno='(212) 555-7334'
+          email="mr.fluffykit@meow.com"
+          />
+          <Contacts
+          img={require("./images/cattycat.jpg")}
+          name="Mr Reign"
+          phoneno='(212) 555-2354'
+          email="mr.reigncat@meow.com"
+          
+          />
+          <Contacts
+            img={require('./images/garf.jpg')}
+            name="Mr Cattycat"
+            phoneno='(212) 555-9291'
+            email="mr.cattykat@meow.com"
+          />
+
+        </div>
+        
       </div>
       
     </div>
---
+++ src/Components/Contact.js
@@ -0,0 +1,28 @@
+import { Card, CardBody,CardImg,CardHeader, Badge, CardImgOverlay  } from "reactstrap"
+import cat from "../images/cat 1.jpg"
+export default function Contacts(props){
+    console.log(props)
+    
+    return(
+        <div className=" col-12 col-sm-6 col-lg-3 mx-auto">
+            <div className="row">
+            <Card className=" my-3">
+                <CardImg src={props.img} className="cat-image align-self-center"/>
+                
+                <CardBody className="row">
+                    <h3><strong>{props.name}</strong></h3>
+                    <p>
+                        <span ><i class="fa-solid fa-phone"></i> {props.phoneno}</span>
+                    </p>
+                    <p>
+                        <span ><i class="fa-solid fa-envelope"></i> {props.email}</span>
+                    </p>
+                </CardBody>
+            </Card>
+
+            
+            </div>
+        </div>
+
+    )
+}
